835b2c6689ad42fdb96b4f4cde35d0bf897b2da6
Aldson10 <119904513+Aldson10@users.noreply.github.com>
2022-12-06 19:46:49 -0300

    Update app.py

--- App/app.py
+++ App/app.py
@@ -5,7 +5,7 @@ import numpy as np
 st.title("Web App Football Data")
 
 st.sidebar.header("Leagues")
-selected_league = st.sidebar.selectbox('League',['England','Germany','Italy','Spain','France','Scotland','Netherlands','Portugal','Turkey'])
+selected_league = st.sidebar.selectbox('League',['England','Germany','Italy','Spain','France','Scotland','Netherlands','Portugal','Turkey','Brazil'])
 
 st.sidebar.header("Season")
 selected_season = st.sidebar.selectbox('Season', ['2021/2022','2020/2021','2019/2020'])
@@ -31,6 +31,8 @@ def load_data(league, season):
     league = 'Pl'
   if selected_league == 'Turkey':
     league = 'Tl'
+  if selected_league == 'Brazil':
+    league = 'BRA'
    
   if selected_season == '2021/2022':
     season = '2122'
